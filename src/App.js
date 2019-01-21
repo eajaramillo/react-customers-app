fda2dc43e814269d64d5023df986d2b111e4e081
Edwar Jaramillo <eajs565@gmail.com>
2019-01-21 10:20:27 -0500

    Route and Switch
    Route and Switch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import { Link, BrowserRouter as Router } from 'react-router-dom';
+import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  renderHome = () => <h1>Home</h1>;
+
+  renderCustomerContainer = () => <h1>Customer Container</h1>;
+
+  renderCustomerListContainer = () => <h1>Customer List Container</h1>;
+
+  renderCustomerNewContainer = () => <h1>Customer New Container</h1>;
+  
   render() {
     return (
       <Router>
         <div className="App">
-          <Link to="/customers">Customers</Link><br />
-          <Link to="/customers/30000000">Customers</Link>
+          <Route path="/customers/new" component={this.renderCustomerNewContainer} />
+          <Route path="/customers/:dni" component={this.renderCustomerContainer} />
+          <Switch>
+            <Route path="/customers" component={this.renderCustomerListContainer} />
+            <Route path="/" component={this.renderHome} />
+          </Switch>
         </div>
       </Router>
     );
